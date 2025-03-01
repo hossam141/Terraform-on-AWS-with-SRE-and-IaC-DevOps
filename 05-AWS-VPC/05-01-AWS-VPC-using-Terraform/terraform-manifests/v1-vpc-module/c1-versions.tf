dd336c06a7ec3f62cc21335793ba21a58e984b3f
Hossam Taha <hossamtaha998@outlook.com>
2025-03-02 01:43:07 +0200

    Design a 3 Tier AWS VPC with NAT Gateways using Terraform

--- 05-AWS-VPC/05-01-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c1-versions.tf
+++ 05-AWS-VPC/05-01-AWS-VPC-using-Terraform/terraform-manifests/v1-vpc-module/c1-versions.tf
@@ -1,13 +1,9 @@
 # Terraform Block
 terraform {
-  required_version = ">= 1.6"
+  required_version = ">= 0.14"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 5.0"
-    }
-    null = {
-      source = "hashicorp/null"
       version = "~> 3.0"
     }        
   }
