c2ff105c69c93df98c27478a152228a1c1ba5cae
Hossam Taha <hossamtaha998@outlook.com>
2025-03-03 00:02:30 +0200

    Security Group for Private EC2 Instances

--- 06-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c5-04-securitygroup-privatesg.tf
+++ 06-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c5-04-securitygroup-privatesg.tf
@@ -1,18 +1,18 @@
 # AWS EC2 Security Group Terraform Module
-# Security Group for Public Bastion Host
+# Security Group for Private EC2 Instances
 
-module "public_bastion_sg" {
+module "private_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
 
-  name = "public_bastion_sg"
+  name = "private_sg"
   description = "Security Group with SSH port open for everybody (IPv4 CIDR), egress ports are all world open"
 
   vpc_id = module.vpc.vpc_id
 
   # Ingress Rules & CIDR Blocks
-  ingress_rules = ["ssh-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_rules = ["ssh-tcp", "http-80-tcp"]
+  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
   # Egress Rule - all-all open
   egress_rules = ["all-all"]
 
