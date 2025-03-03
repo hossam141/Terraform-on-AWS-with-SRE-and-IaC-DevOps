a54ed24cad7d9093a816eee22531de9f227a3608
Hossam Taha <hossamtaha998@outlook.com>
2025-03-03 03:36:21 +0200

    upgade EC2 module

--- 06-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c7-03-ec2instance-bastion.tf
+++ 06-AWS-EC2Instance-and-SecurityGroups/terraform-manifests/c7-03-ec2instance-bastion.tf
@@ -2,7 +2,7 @@
 # Bastion Host - EC2 Instance that will be created in VPC Public Subnet
 module "ec2_public" {
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "2.17.0"
+  version = "5.5.0"
   # insert the 10 required variables here
   name                   = "${var.environment}-BastionHost"
   ami                    = data.aws_ami.amzlinux2.id
