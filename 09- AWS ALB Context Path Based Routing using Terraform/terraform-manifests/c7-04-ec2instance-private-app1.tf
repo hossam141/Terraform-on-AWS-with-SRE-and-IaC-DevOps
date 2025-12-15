921ff97f476c5ab98a1a10a0f0bf13856992751c
Hossam Taha <hossamtaha998@outlook.com>
2025-12-15 20:30:32 +0200

    ec2_private_app

--- 09- AWS ALB Context Path Based Routing using Terraform/terraform-manifests/c7-04-ec2instance-private-app1.tf
+++ 09- AWS ALB Context Path Based Routing using Terraform/terraform-manifests/c7-04-ec2instance-private-app1.tf
@@ -1,18 +1,16 @@
 # AWS EC2 Instance Terraform Module
-# EC2 Instances that will be created in VPC Private Subnets
-module "ec2_private" {
+# EC2 Instances that will be created in VPC Private Subnets for App1
+module "ec2_private_app1" {
   depends_on = [ module.vpc ] # VERY VERY IMPORTANT else userdata webserver provisioning will fail
   source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "5.5.0"
-  # insert the 10 required variables here
-  name                   = "${var.environment}-vm"
+  version = "5.5.0"    
+  name                   = "${var.environment}-app1"
   ami                    = data.aws_ami.amzlinux2.id
   instance_type          = var.instance_type
   key_name               = var.instance_keypair
   user_data = file("${path.module}/app1-install.sh")
-  vpc_security_group_ids = [module.private_sg.security_group_id]
-  for_each = toset(["0", "1"])
-  subnet_id = element(module.vpc.private_subnets, tonumber(each.key))
-
   tags = local.common_tags
+  for_each = toset(["0", "1"])
+  subnet_id =  element(module.vpc.private_subnets, tonumber(each.key))
+  vpc_security_group_ids = [module.private_sg.security_group_id]
 }
---
+++ 09- AWS ALB Context Path Based Routing using Terraform/terraform-manifests/c7-05-ec2instance-private-app2.tf
@@ -0,0 +1,16 @@
+# AWS EC2 Instance Terraform Module
+# EC2 Instances that will be created in VPC Private Subnets for App2
+module "ec2_private_app2" {
+  depends_on = [ module.vpc ] # VERY VERY IMPORTANT else userdata webserver provisioning will fail
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "5.5.0"    
+  name                   = "${var.environment}-app2"
+  ami                    = data.aws_ami.amzlinux2.id
+  instance_type          = var.instance_type
+  key_name               = var.instance_keypair
+  user_data = file("${path.module}/app2-install.sh")
+  tags = local.common_tags
+  for_each = toset(["0", "1"])
+  subnet_id =  element(module.vpc.private_subnets, tonumber(each.key))
+  vpc_security_group_ids = [module.private_sg.security_group_id]
+}
