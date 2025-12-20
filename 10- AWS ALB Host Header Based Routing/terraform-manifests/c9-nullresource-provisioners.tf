eca6f6416c1768b9d552e1497cca43e268d5823f
Hossam Taha <hossamtaha998@outlook.com>
2025-12-20 02:06:15 +0200

    ALB-Host-Header-Based-Routing

--- 10- AWS ALB Host Header Based Routing/terraform-manifests/c9-nullresource-provisioners.tf
+++ 10- AWS ALB Host Header Based Routing/terraform-manifests/c9-nullresource-provisioners.tf
@@ -27,7 +27,17 @@ resource "null_resource" "name" {
     command = "echo VPC created on `date` and VPC ID: ${module.vpc.vpc_id} >> creation-time-vpc-id.txt"
     working_dir = "local-exec-output-files/"
     #on_failure = continue
-  } 
+
+  ## Local Exec Provisioner:  local-exec provisioner (Destroy-Time Provisioner - Triggered during deletion of Resource)
+/*  provisioner "local-exec" {
+    command = "echo Destroy time prov `date` >> destroy-time-prov.txt"
+    working_dir = "local-exec-output-files/"
+    when = destroy
+    #on_failure = continue
+  }  
+  */
+
+}
 
 # Creation Time Provisioners - By default they are created during resource creations (terraform apply)
 # Destory Time Provisioners - Will be executed during "terraform destroy" command (when = destroy)  
